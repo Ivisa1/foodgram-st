d5de75d78c600b338cb19bdc5430eff79acd95cc
Ivisa1 <114678764+Ivisa1@users.noreply.github.com>
2025-05-24 17:26:55 +0300

    исправлена короткая ссылка

--- backend/foodgram_api/recipes/urls.py
+++ backend/foodgram_api/recipes/urls.py
@@ -5,5 +5,5 @@ from recipes.views import recipe_link
 app_name = 'recipes'
 
 urlpatterns = [
-    path('s/<int:recipe_id>/', recipe_link, name='get_recipe_link'),
+    path('recipes/<int:recipe_id>/', recipe_link, name='get_recipe_link'),
 ]
